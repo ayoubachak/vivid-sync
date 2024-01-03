35236b0eb18838f173eb30bde2402080f7196d39
Ayoub Achak <ayoub.achak01@gmail.com>
2024-01-03 15:51:04 +0100

    Delete unused account-related files and code

--- accounts/instagram/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import InitiateInstagramAuth, InstagramCallback
-
-urlpatterns = [
-    path('auth/init/', InitiateInstagramAuth.as_view(), name='initiate-instagram-auth'),
-    path('login/callback/', InstagramCallback.as_view(), name='instagram-callback'),
-]
--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-     path('instagram/', include('accounts.instagram.urls')),
-]
